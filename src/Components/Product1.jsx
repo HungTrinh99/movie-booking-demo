09fdc7039fee36deef668d5ebd169a49a0145160
HungTrinh99 <minhhung.it99@gmail.com>
2020-05-19 13:11:33 +0700

    first commit

---
+++ src/Components/Product1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import style from './Product1.css'
+export default function Product1() {
+    return (
+        <div>
+            <p className="title">This is Product1</p>
+        </div>
+    )
+}
